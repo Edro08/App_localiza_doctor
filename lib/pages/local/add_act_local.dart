06270d8df025e72d2e2ef292fa9549fa1d1adea5
Moises Fabián Cruz <94415134+fabianmoises78@users.noreply.github.com>
2022-10-18 09:24:39 -0600

    vista de hospitales

---
+++ lib/pages/local/add_act_local.dart
@@ -0,0 +1,170 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_feather_icons/flutter_feather_icons.dart';
+
+class adic_actu_local extends StatefulWidget {
+  const adic_actu_local({Key? key, this.index}) : super(key: key);
+  final dynamic index;
+  //1 es para añadir nuevo registro
+  //2 es para actualizar un registro ya existente
+
+  @override
+  State<adic_actu_local> createState() => _adic_actu_localState();
+}
+
+class _adic_actu_localState extends State<adic_actu_local> {
+  List<String> listFiltro = <String>[
+    'Onedhddgdgdgdgdgd',
+    'Two',
+    'Three',
+    'Four'
+  ];
+  late String dropdownValue;
+
+  @override
+  void initState() {
+    dropdownValue = listFiltro.first;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+
+  Body() {
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          appBarAdicionarHospital(),
+          AgregarCodigo(),
+          AgregarNombreHospital(),
+          Listado(),
+          DireccionHospital()
+        ],
+      ),
+    );
+  }
+
+  Widget AgregarCodigo() {
+    return Padding(
+      padding: const EdgeInsets.only(left: 20, right: 20, bottom: 15, top: 10),
+      child: Container(
+        child: TextField(
+          decoration: InputDecoration(
+            labelText: 'Codigo hospital',
+            fillColor: Colors.white,
+            filled: true,
+            border: OutlineInputBorder(),
+            hintText: 'Ingrese Código',
+          ),
+          style: TextStyle(
+              fontSize: 13, fontWeight: FontWeight.normal, color: Colors.black),
+        ),
+      ),
+    );
+  }
+
+  Widget AgregarNombreHospital() {
+    return Padding(
+      padding: const EdgeInsets.only(left: 20, right: 20, bottom: 15, top: 10),
+      child: Container(
+        child: TextField(
+          decoration: InputDecoration(
+            labelText: 'Nombre Hospital',
+            fillColor: Colors.white,
+            filled: true,
+            border: OutlineInputBorder(),
+            hintText: 'Ingrese Nombre del Hospital/Clinica',
+          ),
+          style: TextStyle(
+              fontSize: 13, fontWeight: FontWeight.normal, color: Colors.black),
+        ),
+      ),
+    );
+  }
+
+  Widget Listado() {
+    return Container(
+      width: MediaQuery.of(context).size.width / 1.4,
+      height: 50,
+      decoration: BoxDecoration(border: Border.all(color: Colors.amber)),
+      child: Padding(
+        padding: const EdgeInsets.only(left: 8, right: 8),
+        child: DropdownButton<String>(
+          value: dropdownValue,
+          elevation: 16,
+          style: const TextStyle(color: Colors.black, fontSize: 16),
+          icon: const Icon(Icons.arrow_drop_down),
+          dropdownColor: Colors.amber,
+          underline: Container(
+            height: 0,
+          ),
+          onChanged: (String? value) {
+            setState(() {
+              dropdownValue = value!;
+            });
+          },
+          items: listFiltro.map<DropdownMenuItem<String>>((String value) {
+            return DropdownMenuItem<String>(
+              value: value,
+              child: Text(value),
+            );
+          }).toList(),
+        ),
+      ),
+    );
+  }
+
+  Widget DireccionHospital() {
+    return Padding(
+      padding: const EdgeInsets.only(left: 20, right: 20, bottom: 15, top: 10),
+      child: Container(
+        child: TextField(
+          decoration: InputDecoration(
+            labelText: 'Dirección',
+            fillColor: Colors.white,
+            filled: true,
+            border: OutlineInputBorder(),
+            hintText: 'Ingrese Dirección',
+          ),
+          style: TextStyle(
+              fontSize: 13, fontWeight: FontWeight.normal, color: Colors.black),
+        ),
+      ),
+    );
+  }
+
+  appBarAdicionarHospital() {
+    return AppBar(
+      automaticallyImplyLeading: false,
+      backgroundColor: Colors.transparent,
+      elevation: 0,
+      title: Padding(
+          padding: const EdgeInsets.only(left: 0, right: 10),
+          child:
+              Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+            Text(
+              widget.index == 1
+                  ? "Adicionar Hospital"
+                  : widget.index == 2
+                      ? "Actualizar Hospital"
+                      : "",
+              style:
+                  TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+            ),
+            MaterialButton(
+              minWidth: 30.0,
+              height: 40.0,
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              color: Colors.amber,
+              child: const Icon(
+                FeatherIcons.logOut,
+                color: Colors.white,
+              ),
+            ),
+          ])),
+    );
+  }
+}
--- lib/pages/local/listado_local.dart
+++ lib/pages/local/listado_local.dart
@@ -8,6 +8,15 @@ class ListadoLocal extends StatefulWidget {
 }
 
 class _ListadoLocalState extends State<ListadoLocal> {
+  List<String> listFiltro = <String>['One', 'Two', 'Three', 'Four'];
+  late String dropdownValue;
+
+  @override
+  void initState() {
+    dropdownValue = listFiltro.first;
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container();
